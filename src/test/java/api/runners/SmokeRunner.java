461c6bf9f5091eb9d7725ba9d7e27bd47ba23590
Neelofar Muheb <nilofar.muheb0@gmail.com>
2023-05-25 02:05:24 -0400

    parameterized pom for runner

--- src/test/java/api/runners/SmokeRunner.java
+++ src/test/java/api/runners/SmokeRunner.java
@@ -5,10 +5,9 @@ import com.intuit.karate.junit5.Karate;
 public class SmokeRunner {
 	@Karate.Test
 	public Karate runTests() {
-		//run() method required path to feature files
-		//you can use tags() method to specify the Tags.
 		return Karate.run("classpath:feature")
-		.tags("Smoke" , "Regression");
+				.tags("Smoke");
+		
 	}
 
 }
